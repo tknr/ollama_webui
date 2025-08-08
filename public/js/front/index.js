9c914c3f63e5857d4722aa0f535da58b916d6813
tknr <geishagirlskoume@gmail.com>
2025-08-08 15:51:42 +0900

    changed promtp stream to chat

--- public/js/front/index.js
+++ public/js/front/index.js
@@ -52,7 +52,7 @@ function getModels() {
 function renderPromptStream() {
     $send.on("click", async (event) => {
         let OLLAMA_MODEL = $('[name=model]').val();
-        
+
         let input_val = $input.val();
         let right_id = uuidv4();
         $output.prepend(POST_YOU
@@ -70,24 +70,22 @@ function renderPromptStream() {
             .replaceAll('#TEXT#', '')
         );
         $('#' + left_id).autosize();
-        
+
         ollama_instance = new Ollama({
-            model: OLLAMA_MODEL,
             url: "/ollama/api/",
         });
-        ollama_instance.prompt_stream(input_val, function (error, response) {
-            if (error) {
-                console.error(error)
-            }
-            else if (response.done) {
-                // console.log('response.done:' + response.done);
-            }
-            else {
-                console.log(response);
-                $('#' + left_id).val($('#' + left_id).val() + response.response);
-                $('#' + left_id).trigger('autosize.resize');
-            }
+        const message = { role: "user", content: input_val };
+        const response = await ollama.chat({
+            model: OLLAMA_MODEL,
+            messages: [message],
+            stream: true,
         });
+        for await (const part of response) {
+            console.log(part)
+            process.stdout.write(part.message.content);
+            $('#' + left_id).val($('#' + left_id).val() + part.message.content);
+            $('#' + left_id).trigger('autosize.resize');
+        }
     });
 }
 
